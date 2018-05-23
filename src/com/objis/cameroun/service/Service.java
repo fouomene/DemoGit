13622f5f95f3414528f35d47a54b4b68bb121772
sonilyn <sonialynda@gmail.com>
2018-05-23 20:53:33 +0100

    implementattion de la methode perimetreRectangle

--- src/com/objis/cameroun/service/Service.java
+++ src/com/objis/cameroun/service/Service.java
@@ -30,10 +30,15 @@ public class Service implements IService {
 		return 0;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.objis.cameroun.service.IService#perimetreRectangle(int, int)
+	 * @author Sonia
+	 * Methode perimetreRectangle prend en parametre deux entiers et retourne le perimetre d'un rectangle 
+	 */
 	@Override
 	public int perimetreRectangle(int longeur, int largeur) {
-		// TODO Auto-generated method stub
-		return 0;
+		int perimetre = (longeur + largeur) * 2;
+		return perimetre;
 	}
 
 	@Override
